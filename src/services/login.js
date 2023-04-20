6ff87a0acf193d8f0d675a49aa3f877a4428f8b7
prnvjn <prnvjn@gmail.com>
2023-04-19 21:24:37 -0500

    Frontend Login

---
+++ src/services/login.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:8000/login'
+
+const login = async credentials => {
+    console.log(credentials,"values")
+  const response = await axios.post(baseUrl, credentials)
+  console.log(response)
+  return response.data
+}
+
+export default { login }
